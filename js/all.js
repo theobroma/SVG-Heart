16bbcb81397e08c752a7047df946d14a270ed327
Aleksandr Siryi <theobroma222@gmail.com>
2017-12-21 19:26:50 +0200

    first node

--- js/all.js
+++ js/all.js
@@ -1,12 +1,45 @@
-// First lets create our drawing surface out of existing SVG element
-// If you want to create new surface just provide dimensions
-// like s = Snap(800, 600);
-var s = Snap('#svg');
-// Lets create big circle in the middle:
-var bigCircle = s.circle(150, 150, 100);
+const paper = Snap('#preloader__logo');
 // By default its black, lets change its attributes
-bigCircle.attr({
-  fill: '#bada55',
-  stroke: '#000',
-  strokeWidth: 5
-});
+// paper.attr({
+//   fill: '#bada55',
+//   stroke: '#000',
+//   strokeWidth: 5
+// });
+
+// const path = paper.path('M 10,50 L 100,200');
+const renderNodes = i => {
+  const node = paper.circle(pos[i].x, pos[i].y, 10);
+  node.attr({
+    fill: '#fff'
+  });
+};
+
+const drawPath = i => {
+  const path = paper
+    .path(`M ${pos[i].x}, ${pos[i].y}`)
+    .attr({
+      fill: 'transparent',
+      stroke: '#fff'
+    })
+    .animate(
+      { d: `M ${pos[i].x}, ${pos[i].y} L ${pos[i + 1].x}, ${pos[i + 1].y}` },
+      1000,
+      mina.easeinout,
+      function() {
+        renderNodes(i);
+      }
+    );
+};
+
+pos = [
+  {
+    x: 218,
+    y: 174
+  },
+  {
+    x: 326,
+    y: 170
+  }
+];
+const i = 0;
+drawPath(i);
